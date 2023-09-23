228f2eda15e7e37429940f96a3ff342bc86d5460
mmanevski <markom@tabtale.com>
2023-09-23 18:44:34 +0200

    missions UI

---
+++ Assets/3_Scripts/Missions/MissionButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionButton : MonoBehaviour
+{
+    [SerializeField]
+    Image missionImage;
+    [SerializeField]
+    TextMeshProUGUI missionText;
+    [SerializeField]
+    TextMeshProUGUI missionReward;
+
+    public void SetRewardButton(MissionConfig mission)
+    { 
+        
+    }
+}
---
+++ Assets/3_Scripts/Missions/MissionManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionManager : MonoBehaviour
+{
+
+    [SerializeField]
+    MissionsUI missionsUI;
+
+    public void OnOpenMissionsUI()
+    { 
+        missionsUI.gameObject.SetActive(true);
+    }
+
+    public void OnCloseMissionsUI()
+    {
+        missionsUI.gameObject.SetActive(false); ;
+    }
+}
